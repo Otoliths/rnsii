4d4630363f4a1a1858f37cf6e8f35da22ab565bf
Liuyong Ding <33018532+Otoliths@users.noreply.github.com>
2021-03-25 00:07:39 +0800

    Add files via upload

--- R/get_nsii.R
+++ R/get_nsii.R
@@ -1,8 +1,9 @@
-#' @title Download data from the NSII
+#' @title Download occurrence data from the NSII
 #' @description The result returns the detailed information of the queried species, including the scientific name, the Latin name, common name, description, latitude and longitude, etc.
 #' @rdname  get_nsii
 #' @name get_nsii
 #' @param query \code{character} Only single Latin name is queried.
+#' @param offset \code{numeric} Query page number.The default value is 0, which corresponds to the first page, each query returns 30.
 #' @return list
 #' @author Liuyong Ding
 #' @details Visit the website \url{http://www.nsii.org.cn} for more details.
@@ -10,25 +11,41 @@
 #' @importFrom jsonlite fromJSON
 #' @examples
 #' \dontrun{
-#' result <- get_nsii(query = "Artemisia argyi")
+#' result <- get_nsii(query = "Artemisia argyi",offset = 0)
 #' str(result)
-#' tibble::tibble(result$Specimens)
+#' tibble::tibble(result$row)
 #' }
 #' @export
-get_nsii <- function(query){
-  cat(sprintf("Download  date: %s",Sys.Date()),sep = "\n")
-  res <- jsonlite::fromJSON(RCurl::postForm(uri = "http://www.nsii.org.cn/2017/API/nsiimap.php",name = query))
-  if(res$Status == "ok"){
-    cat(sprintf("Query successfully: %s",query), sep = "\n")
-  }
-  if("Title" %in% names(res)){
-    res[["Title"]] <- gsub("<.*?>", "", res[["Title"]])
-  }
-  if(length(res$Specimens) > 0 ){
-    cat(sprintf("Include latitude and longitude: %s",query), sep = "\n")
-  }else{
-    cat(sprintf("Exclude latitude and longitude: %s",query), sep = "\n")
-  }
-  return(res)
 
+get_nsii <- function(query,offset = 0){
+  query <- gsub(" ","%20",query)
+  url <- paste0("https://www.cvh.ac.cn/controller/spms/list.php?&taxonName=",query,"&offset=",offset)
+  x <- httr::GET(url = url,
+                 httr::add_headers("user-agent" = "Mozilla/5.0 (Macintosh; Intel Mac OS X 11_2_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.90 Safari/537.36",
+                                   'referer'= url))
+  res <- jsonlite::fromJSON(content(x, "text"), simplifyVector = FALSE)
+  if(httr::http_status(x)$category == "Success"){
+    cat(sprintf("Query successfully: %s",gsub("%20"," ",query)))
+    cat(sprintf("(%s)",res$total, sep = "\n"))
+  }
+  res$rows <- rlist::list.stack(res$row)
+  return(res)
+}
+
+
+nsii <- function(query){
+  query <- gsub(" ","%20",query)
+  url <- paste0("https://www.cvh.ac.cn/controller/spms/list.php?&taxonName=",query)
+  x <- httr::GET(url = url,
+                 httr::add_headers("user-agent" = "Mozilla/5.0 (Macintosh; Intel Mac OS X 11_2_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.90 Safari/537.36",
+                                   'referer'= url))
+  res <- jsonlite::fromJSON(content(x, "text"), simplifyVector = FALSE)
+  if(httr::http_status(x)$category == "Success"){
+    cat(sprintf("Query successfully: %s",gsub("%20"," ",query)))
+    cat(sprintf("(%s)",res$total, sep = "\n"))
+  }
+  result <- lapply(seq(0,(ceiling(res$total/30)*10),10),function(i){
+  get_nsii(query = query,offset = i)
+})
+  return(result)
 }
---
+++ R/get_occ.R
@@ -0,0 +1,34 @@
+#' @title Download occurrence data from the NSII
+#' @description The result returns the detailed information of the queried species, including the scientific name, the Latin name, common name, description, latitude and longitude, etc.
+#' @rdname  get_occ
+#' @name get_occ
+#' @param query \code{character} Only single Latin name is queried.
+#' @return list
+#' @author Liuyong Ding
+#' @details Visit the website \url{http://www.nsii.org.cn} for more details.
+#' @importFrom RCurl postForm
+#' @importFrom jsonlite fromJSON
+#' @examples
+#' \dontrun{
+#' result <- get_occ(query = "Artemisia argyi")
+#' str(result)
+#' tibble::tibble(result$Specimens)
+#' }
+#' @export
+get_occ <- function(query){
+  cat(sprintf("Download  date: %s",Sys.Date()),sep = "\n")
+  res <- jsonlite::fromJSON(RCurl::postForm(uri = "http://www.nsii.org.cn/2017/API/nsiimap.php",name = query),flatten = TRUE)
+  if(res$Status == "ok"){
+    cat(sprintf("Query successfully: %s",query), sep = "\n")
+  }
+  if("Title" %in% names(res)){
+    res[["Title"]] <- gsub("<.*?>", "", res[["Title"]])
+  }
+  if(length(res$Specimens) > 0 ){
+    cat(sprintf("Include latitude and longitude: %s",query), sep = "\n")
+  }else{
+    cat(sprintf("Exclude latitude and longitude: %s",query), sep = "\n")
+  }
+  return(res)
+
+}
