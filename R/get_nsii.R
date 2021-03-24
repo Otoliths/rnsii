a863b9dfabb1fe4bb30b54424a0072cf79d04f55
Liuyong Ding <33018532+Otoliths@users.noreply.github.com>
2021-03-25 00:25:01 +0800

    Add files via upload

--- R/get_nsii.R
+++ R/get_nsii.R
@@ -1,5 +1,5 @@
-#' @title Download occurrence data from the NSII
-#' @description The result returns the detailed information of the queried species, including the scientific name, the Latin name, common name, description, latitude and longitude, etc.
+#' @title Download specimen data from the NSII
+#' @description The result returns the detailed information of the queried species, including the scientific name, the Latin name, common name, description, etc.
 #' @rdname  get_nsii
 #' @name get_nsii
 #' @param query \code{character} Only single Latin name is queried.
@@ -9,6 +9,7 @@
 #' @details Visit the website \url{http://www.nsii.org.cn} for more details.
 #' @importFrom httr GET
 #' @importFrom httr add_headers
+#' @importFrom httr content
 #' @importFrom jsonlite fromJSON
 #' @importFrom rlist list.stack
 #' @examples
@@ -26,12 +27,13 @@ get_nsii <- function(query,offset = 0){
   x <- httr::GET(url = url,
                  httr::add_headers("user-agent" = "Mozilla/5.0 (Macintosh; Intel Mac OS X 11_2_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.90 Safari/537.36",
                                    'referer'= url))
-  res <- jsonlite::fromJSON(content(x, "text"), simplifyVector = FALSE)
+  res <- jsonlite::fromJSON(httr::content(x, "text"), simplifyVector = FALSE)
   if(httr::http_status(x)$category == "Success"){
     cat(sprintf("Query successfully: %s",gsub("%20"," ",query)))
     cat(sprintf("(%s)",res$total, sep = "\n"))
   }
   res$rows <- rlist::list.stack(res$row)
+  names(res)[5] <- "data"
   return(res)
 }
 
